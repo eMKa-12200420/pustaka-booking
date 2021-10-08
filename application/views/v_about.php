453b0206e08e6336e6e72a4ea71e0dc4deadc0d2
eMKa-12200420 <12200420@bsi.ac.id>
2021-10-08 10:02:08 +0700

    Membuat template web sederhana

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Muhammad</li>
+        <li>Nama Belakang : Marifatullah</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jl. Brawijaya, Kasihan, Bantul</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Yogyakarta</li>
+    </ul>
+    
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+    </ul>
+</section>
